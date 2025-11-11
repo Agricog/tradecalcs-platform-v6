83c8c230e0962750dfebaee31b1e4f2391cd9400
Agricog <mick@mickstevenson.com>
2025-11-11 19:41:29 +0000

    Update BrickBlockCalculatorPage.tsx

--- src/pages/BrickBlockCalculatorPage.tsx
+++ src/pages/BrickBlockCalculatorPage.tsx
@@ -1,5 +1,17 @@
 import BrickBlockCalculator from '../components/BrickBlockCalculator'
+import SEO from '../components/SEO'
 
 export default function BrickBlockCalculatorPage() {
-  return <BrickBlockCalculator />
+  return (
+    <>
+      <SEO
+        title="Brick & Block Calculator | UK Bricklayer Material Calculator"
+        description="Calculate bricks, concrete blocks (4\" & 6\"), mortar & cement needed for walls. Free UK calculator for bricklayers with mortar mix ratios."
+        keywords="brick calculator, block calculator, concrete block, mortar calculator, bricklayer tools, UK construction"
+        url="https://tradecalcs.co.uk/brick-block-calculator"
+      />
+      <BrickBlockCalculator />
+    </>
+  )
 }
+
