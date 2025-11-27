4dadc67aca82f2cbe3c2ad3e6567c2cceccecc2b
Agricog <mick@mickstevenson.com>
2025-11-27 18:09:10 +0000

    Update InsulationCalculator.tsx

--- src/components/InsulationCalculator.tsx
+++ src/components/InsulationCalculator.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
-import { Info, CheckCircle2, HelpCircle, Layers, AlertCircle } from 'lucide-react'
+import { Info, CheckCircle2, Layers, AlertCircle } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import QuoteGenerator from './QuoteGenerator'
 
