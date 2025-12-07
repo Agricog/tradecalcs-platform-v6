a06655783c6b731640f363709b454c3c2c5f2fce
Agricog <mick@mickstevenson.com>
2025-12-07 14:25:18 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'grid-cols-3',
+    'lg:grid-cols-3',
+    'md:grid-cols-2',
+  ],
   theme: { extend: {} },
   plugins: [],
 }
+
