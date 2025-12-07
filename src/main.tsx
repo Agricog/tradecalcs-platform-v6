754fe0a9a3d6bd0ff6fb64c56446c569d4328d05
Agricog <mick@mickstevenson.com>
2025-12-07 17:27:14 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App'
 import './index.css'
+import { registerServiceWorker, setupConnectionListener } from './sw-register'
+
+// Register Service Worker for offline support
+registerServiceWorker()
+
+// Listen to connection changes
+setupConnectionListener((online: boolean) => {
+  const status = online ? 'ONLINE' : 'OFFLINE'
+  console.log(`📡 Connection status: ${status}`)
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 )
+
